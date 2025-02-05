e9bb353a4edbabd6a347e81f813c1b3174349522
matinslcs <ccplucasmartins@gmail.com>
2025-02-04 22:38:55 -0300

    Primeiro commit

---
+++ Aprendizado .NET Core/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Aprendizado_.NET_Core
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teste!");
+        }
+    }
+}
